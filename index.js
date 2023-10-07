efc01c9fbbb5d85a070b003b11cfaaf40663f7ec
Ali-Stranger <136221894+Ali-Stranger@users.noreply.github.com>
2023-10-08 01:17:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+console.log('HELLO WORLD')
+console.log('PAKISTAN ZINDABAD')
+let message ='hello world';
+console.log(message)
+let message2 ='i got admission';
+console.log(message2)
+// mathematical calculation
+let number = 333
+let statment3 = `${number} is a greater number`
+console.log(statment3) 
+//arrays
+const numberarray = [1,2,3,4,5]
+console.log(numberarray)
+const numberofarrays = [1,2,3,4,5]
+numberofarrays.push(75)
+console.log(numberofarrays)
+console.log(numberofarrays.length)
+numberofarrays.pop()
+console.log(numberofarrays)
+const arrayofallrounders = [1,"ali",true,[5,2,3,4]]
+console.log (arrayofallrounders)
+//objects
+const first_object = {
+    name:'ali',
+    rollnumber:1,
+    attendance:true,
+    scores:[5,5,6,7],
+    course_score:{
+        js:6,
+        html:5,
+    }
+}
+console.log(first_object.name)
+delete first_object.attendance
+console.log(first_object)
+first_object.marks = {
+    css:10,
+    python:0,
+}
+console.log(first_object)
+console.log(first_object.course_score.html)
+//conditions programming
+let weather = 'rain'
+if (weather==='rain'){console.log('umbrella is reuired')
+}else{console.log('nothing is required')}
+//and operator
+let math = 5
+let phy = 10
+if (math>5,phy>5) {
+    console.log('pass')
+}else {console.log=('fail')}
+//differenc between == & ===
+let value = 5
+if (value=='5') console.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
